1a3a274c98cfb32eb6a7f5371c5f15a7134e1b6c
Ravshan Oromov <you@example.com>
2020-11-19 04:44:47 +0900

    first commit

---
+++ class1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name="ravshan";
+  String surname="oromov";
+  int age=22;
+  String education="university student";
+  print("my name is $name $surname \nmy age is $age \nIm an $education" );
+}
+
